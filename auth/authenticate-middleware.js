ad845530156243e46da32db27061a163b2e8c24d
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-12-14 10:32:55 -0800

    Register and Login functional.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (req.decodedJwt) {
+    next();
+  } else if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+      if (err) {
+        res.status(401).json({ message: "no dice" });
+      } else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
